4cee20ecba8e7999fd9de8898d8a6b5ad958d5fb
Saiful Fitri <saifit2787@gmail.com>
2022-07-21 13:37:43 +0700

    Belajar Laravel video 2 (WPU sandhika Galih)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Saifit2787 Blog | Home</title>
+</head>
+<body>
+    <h1> Halaman Home test</h1>
+    <h3> Saiful fitri</h3>
+    <p> saifit2787@gmail.com</p>
+    <img src="img/saifit2787.jpg" alt="saifit2787" width="200">
+
+    {{-- <script src="js.script.js"></script> --}}
+    <script src="js/script.js"></script>
+</body>
+</html>
